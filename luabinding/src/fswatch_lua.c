e6c99178135b1ebb19b3d8adc9a40498e4234097
twinklinggu <twinkling.gu@gmail.com>
2023-04-15 23:27:35 +0800

    add skynet luabinding

--- luabinding/src/fswatch_lua.c
+++ luabinding/src/fswatch_lua.c
@@ -8,11 +8,24 @@
 #include <libfswatch/c/libfswatch.h>
 #include <pthread.h>
 #include <unistd.h>
-// #include "skynet_server.h"
-// #include "skynet_mq.h"
+#include <string.h>
+#include "skynet_server.h"
+#include "skynet_mq.h"
+#include "skynet.h"
 
+#define MAX_SESSION_PATH 64
+#define MAX_SESSION_FLAGS 64
 struct fsw_session_context {
-	FSW_HANDLE handle;	
+  	bool allow_overflow;
+  	bool recursive;
+  	bool directory_only;
+  	bool follow_symlinks;
+	char * paths[MAX_SESSION_PATH];
+	int path_n;
+	int opaque;
+  	double latency;
+	int flags[MAX_SESSION_FLAGS];
+	int flag_n;
 };
 
 /**
@@ -24,102 +37,170 @@ struct fsw_session_context {
  * @param event_num
  * @param data
  */
-void my_callback(fsw_cevent const *const events,
+void event_callback(fsw_cevent const *const events,
                  const unsigned int event_num,
                  void *data)
 {
-  printf("my_callback: %d\n", event_num);
   for(int i = 0; i < event_num; i++) {
-	fsw_cevent const cevent = events[i];	
-	printf("file change, event:");
-	for (int n = 0; n < cevent.flags_num; n++) {
-		printf("%s,", fsw_get_event_flag_name(cevent.flags[n]));
+	fsw_cevent const * event = &events[i];	
+	char * path = event->path;
+	size_t path_len = strlen(path);
+	int *opaque = (int*) data;
+	for (int n = 0; n < event->flags_num; n++) {
+		char * event_flag_name = fsw_get_event_flag_name(event->flags[n]);
+		size_t sz = path_len + strlen(event_flag_name) + 2;
+		char * data = (char *) skynet_malloc(sz);
+		memcpy(data, path, path_len);
+		data[path_len] = ',';
+		memcpy(data + path_len + 1, event_flag_name, strlen(event_flag_name));
+		data[sz -1] = '\0';
+
+		struct skynet_message message;
+		message.source = 0;
+		message.session = 0;
+		message.data = data;
+		message.sz = sz | ((size_t)PTYPE_TEXT << MESSAGE_TYPE_SHIFT);
+		if (skynet_context_push((uint32_t)*opaque, &message))
+		{
+			skynet_free(data);
+		}
 	}
-	printf(" path %s\n", cevent.path);
   }
 }
 
 void *start_monitor(void *param)
 {
-  FSW_HANDLE handle = (FSW_HANDLE) param;
+	struct fsw_session_context *ctx = (struct fsw_session_context *) param;
+	FSW_HANDLE handle = fsw_init_session(system_default_monitor_type);
+	fsw_set_recursive(handle, ctx->recursive);
+	fsw_set_latency(handle, ctx->latency);
+	fsw_set_directory_only(handle, ctx->directory_only);
+	fsw_set_follow_symlinks(handle, ctx->follow_symlinks);
+	for (int i = 0; i < ctx->path_n; i++){
+		fsw_add_path(handle, ctx->paths[i]);
+	}
+	for (int i = 0; i < ctx->flag_n; i++){
+		struct fsw_event_type_filter filter;
+		filter.flag = ctx->flags[i];
+		fsw_add_event_type_filter(handle, filter);
+	}
+	int opaque = ctx->opaque;
+	fsw_set_callback(handle, event_callback, &opaque);
 
-int ret = fsw_start_monitor(handle);
-  if (FSW_OK != ret)
-  {
-    fprintf(stderr, "Error creating thread111 0x%p, %d\n", handle, ret);
-  }
-  else
-  {
-    printf("Monitor stopped \n");
-  }
-
-  return NULL;
-}
-
-static int
-ldestory_session(lua_State *L) {
-	struct fsw_session_context * ctx = lua_touserdata(L, 1);
-	FSW_HANDLE handle = ctx->handle;
-	ctx->handle = NULL;
-	if (FSW_OK != fsw_stop_monitor(handle))
+	printf("fswatch monitor started \n");
+	if (FSW_OK != fsw_start_monitor(handle))
 	{
-		fprintf(stderr, "Error stopping monitor \n");
-		return 0;
+		fprintf(stderr, "Error creating thread\n");
+	}
+	else
+	{
+		printf("Monitor stopped \n");
 	}
 	fsw_destroy_session(handle);
-	return 0;
+	return NULL;
 }
 
 static int
 ladd_path(lua_State *L) {
 	struct fsw_session_context * ctx = lua_touserdata(L, 1);
-	FSW_HANDLE handle = ctx->handle;
-	const char * path = luaL_checkstring(L, 2);
-	if (FSW_OK != fsw_add_path(handle, path))
-    {
-      fsw_last_error();
-	  luaL_error(L, "fsw_add_path failed\n");
-    }
+	if (ctx->path_n < MAX_SESSION_PATH) {
+		const char *path = luaL_checkstring(L, 2);
+		int sz = strlen(path);
+		char *tmp = skynet_malloc(sz +1);
+		memcpy(tmp, path, sz);
+		tmp[sz] = '\0';
+		ctx->paths[ctx->path_n++] = tmp;
+	} else {
+		luaL_error(L, "session paths limit\n");
+	}
 	return 0;
 }
 
 static int
 lstart_monitor(lua_State *L) {
 	struct fsw_session_context * ctx = lua_touserdata(L, 1);
-	FSW_HANDLE handle = ctx->handle;
-
-	if (FSW_OK != fsw_set_callback(handle, my_callback, ctx))
-	{
-	  fsw_last_error();
-	  luaL_error(L, "fsw_set_callback failed\n");
-	}
-	fsw_set_recursive(handle, true);
-	struct fsw_event_type_filter filter;
-	filter.flag = PlatformSpecific; 
-	fsw_add_event_type_filter(handle, filter);
-	pthread_t start_thread;
-	if (pthread_create(&start_thread, NULL, start_monitor, (void *)handle))
+	int sz = sizeof(struct fsw_session_context);
+	struct fsw_session_context * tmp =(struct fsw_session_context *) malloc(sz);
+	memcpy(tmp, ctx, sz);	
+	pthread_t pid;
+	if (pthread_create(&pid, NULL, start_monitor, tmp))
 	{
 	  luaL_error(L, "pthread_create failed\n");
 	}
 	return 0;
 }
 
-luaL_Reg lsession_meta[] = {
-	{"__gc", ldestory_session},
+static int
+lset_recursive(lua_State *L){
+	struct fsw_session_context * ctx = lua_touserdata(L, 1);
+	ctx->recursive =lua_toboolean(L, 2); 
+	return 0;
+}
+
+static int
+lset_directory_only(lua_State *L){
+	struct fsw_session_context * ctx = lua_touserdata(L, 1);
+	ctx->directory_only = lua_toboolean(L, 2);
+	return 0;
+}
+
+static int
+lset_latency(lua_State *L){
+	struct fsw_session_context * ctx = lua_touserdata(L, 1);
+	double latency = luaL_checknumber(L, 2);
+	ctx->latency = latency;
+	return 0;
+}
+
+static int
+lset_follow_symlinks(lua_State *L){
+	struct fsw_session_context * ctx = lua_touserdata(L, 1);
+	ctx->follow_symlinks = lua_toboolean(L, 2);
+	return 0;
+}
+
+static int
+lset_allow_overflow(lua_State *L){
+	struct fsw_session_context * ctx = lua_touserdata(L, 1);
+	ctx->allow_overflow = lua_toboolean(L, 2);
+	return 0;
+}
+
+static int
+ladd_event_type_filter(lua_State *L){
+	struct fsw_session_context * ctx = lua_touserdata(L, 1);
+	enum fsw_event_flag flag = lua_tointeger(L, 2);
+	if (ctx->flag_n < MAX_SESSION_FLAGS)
+	{
+	  ctx->flags[ctx->flag_n++] = flag;
+	}
+	else
+	{
+	  luaL_error(L, "session flags limit");
+	}
+	return 0;
+}
+
+
+luaL_Reg fsw_session_meta[] = {
 	{"add_path", ladd_path},
 	{"start_monitor", lstart_monitor},
+	{"set_recursive", lset_recursive},
+	{"set_directory_only", lset_directory_only},
+	{"set_latency", lset_latency},
+	{"set_follow_symlinks", lset_follow_symlinks},
+	{"set_allow_overflow", lset_allow_overflow},
+	{"add_event_type_filter", ladd_event_type_filter},
 	{NULL, NULL},
 };
 
 static int
 lnew_session(lua_State* L) {
 	struct fsw_session_context * ctx = lua_newuserdatauv(L, sizeof(*ctx), 0);
-	FSW_HANDLE handle = fsw_init_session(inotify_monitor_type);
-	ctx->handle = handle;
-	fprintf(stderr, "creating session 0x%p\n", handle);
-	lua_createtable(L, 0, sizeof(lsession_meta)/sizeof(lsession_meta[0]));
-	luaL_setfuncs(L, lsession_meta, 0);
+	memset(ctx, 0,sizeof(*ctx));
+	ctx->opaque = lua_tointeger(L, 1);
+	lua_createtable(L, 0, sizeof(fsw_session_meta)/sizeof(fsw_session_meta[0]));
+	luaL_setfuncs(L, fsw_session_meta, 0);
 	lua_pushvalue(L, -1);
 	lua_setfield(L, -2, "__index");
 	lua_setmetatable(L, -2);
@@ -133,6 +214,14 @@ linit_library(lua_State* L) {
 		fsw_last_error();
 		luaL_error(L, "libfswatch cannot be initialised!\n");
 	}
+	bool verbose = lua_toboolean(L, 1);
+	return 0;
+}
+
+static int
+lset_verbose(lua_State* L) {
+	bool verbose = lua_toboolean(L, 1);
+	fsw_set_verbose(verbose);
 	return 0;
 }
 
@@ -140,9 +229,10 @@ LUAMOD_API int
 luaopen_fswatch(lua_State *L) {
 	luaL_checkversion(L);
 	luaL_Reg l[] = {
-		{ "init_library", linit_library },
-		{ "new_session", lnew_session },
-		{ NULL, NULL },
+		{"init_library", linit_library},
+		{"new_session", lnew_session},
+		{"set_verbose", lset_verbose},
+		{NULL, NULL},
 	};
 	luaL_newlib(L, l);
 	return 1;
